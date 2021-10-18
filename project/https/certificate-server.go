17e7bd98ca6f4f74674fc87994880fecfdbfa360
Mihhail Sokolov <mihhail1998@gmail.com>
2021-10-18 10:35:18 +0200

    Create certificate server

--- project/https/certificate-server.go
+++ project/https/certificate-server.go
@@ -1,1 +1,14 @@
 package https
+
+import "github.com/gin-gonic/gin"
+
+func RunCertificateServer(certificateFile string, keyFile string) {
+	server := gin.Default()
+	server.GET("/", func(context *gin.Context) {
+		context.Data(200, "text/plain", []byte("Hello World"))
+	})
+	err := server.RunTLS(":5001", certificateFile, keyFile)
+	if err != nil {
+		panic(err)
+	}
+}
